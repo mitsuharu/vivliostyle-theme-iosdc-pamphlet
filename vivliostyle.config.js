599dc83804d2263c5e34ce106a12cea49c7367ef
Mitsuharu Emoto <mthr1982@gmail.com>
2025-06-29 15:52:24 +0900

    add

--- vivliostyle.config.js
+++ vivliostyle.config.js
@@ -3,7 +3,7 @@ module.exports = {
   theme: [
     'node_modules/@vivliostyle/theme-base',
     'node_modules/@vivliostyle/theme-techbook',
-    '@mitsuharu/vivliostyle-theme-noto-sans-jp',
+    'node_modules/@mitsuharu/vivliostyle-theme-noto-sans-jp',
     '.',
   ],
   entry: ['example/default.md'],
